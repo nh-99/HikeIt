fd31903b2e3e3ff0169106902bc98eca96a0f0e2
noah <ifence9@gmail.com>
2015-07-29 02:20:27 -0400

    Initial configuration of settings to use ENV variables

--- HikeIt/settings.py
+++ HikeIt/settings.py
@@ -76,8 +76,10 @@ WSGI_APPLICATION = 'HikeIt.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'hikeit',
+        'USER': os.environ.get("HIKEIT_DB_USER", ''),
+        'PASSWORD': os.environ.get("HIKEIT_DB_PASSWORD", ''),
     }
 }
 
