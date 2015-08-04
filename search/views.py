b84b012c697284fd62bb08a7b9c518d085a87a20
nhthecoder <ifence9@gmail.com>
2015-08-03 21:09:57 -0400

    Add navbar

--- search/views.py
+++ search/views.py
@@ -3,15 +3,19 @@ from django.shortcuts import render
 from trails.models import Trail
 
 def location(request, location):
+	request.session['searchtype'] = 'location'
+	
 	trails = Trail.objects.filter(location__contains=location)[:50]
 	
-	args = {'trails': trails, 'location': location}
+	args = {'trails': trails, 'location': location, 'searchtype': request.session['searchtype']}
 	
 	return render(request, 'search/results.html', args)
 	
 def name(request, name):
+	request.session['searchtype'] = 'name'
+	
 	trails = Trail.objects.filter(name__contains=name)[:50]
 	
-	args = {'trails': trails, 'location': name}
+	args = {'trails': trails, 'location': name, 'searchtype': request.session['searchtype']}
 	
 	return render(request, 'search/results.html', args)
