db5f9270f1a971b9e04efad91dad59c7bf25d7ee
nhthecoder <ifence9@gmail.com>
2015-08-04 23:07:35 -0400

    Jenkins!

--- HikeIt/settings/__init__.py
+++ HikeIt/settings/__init__.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_jenkins',
     'trails',
     'search',
     'static_pages',
