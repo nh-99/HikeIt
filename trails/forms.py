af884a0eceb2f631a79386338a15921d430f31dc
nh-99 <ifence9@gmail.com>
2015-08-12 12:24:48 -0400

    [BUG] Fix a bunch of stuff related to users

--- trails/forms.py
+++ trails/forms.py
@@ -1,5 +1,7 @@
 from django import forms
 
 class TrailImageForm(forms.Form):
-    trail = forms.
+    trail = forms.CharField(max_length=256)
+    lat = forms.CharField(max_length=50)
+    long = forms.CharField(max_length=50)
     image = forms.FileField(label='Select a trail image')
--- users/models.py
+++ users/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.contrib.auth.models import (AbstractBaseUser, BaseUserManager)
+from django.contrib.auth.models import (AbstractBaseUser, BaseUserManager, PermissionsMixin)
 
 class HikeItUserManager(BaseUserManager):
     def create_user(self, email, password, first_name, last_name):
@@ -15,11 +15,12 @@ class HikeItUserManager(BaseUserManager):
         user.set_password(password)
         user.first_name = first_name
         user.last_name = last_name
+        user.is_superuser = True
         user.role = "admin"
         user.save()
         return user
 
-class HikeItUser(AbstractBaseUser):
+class HikeItUser(AbstractBaseUser, PermissionsMixin):
     objects = HikeItUserManager()
     
     username = models.CharField(max_length=254, unique=True, blank=True)
@@ -29,6 +30,16 @@ class HikeItUser(AbstractBaseUser):
     role = models.CharField(max_length=30, default="member")
     image = models.CharField(max_length=200, blank=True)
     token = models.CharField(max_length=300, blank=True)
+    
+    def get_full_name(self):
+        return self.email
+
+    def get_short_name(self):
+        return self.email
+        
+    @property
+    def is_staff(self):
+		return self.role == "admin"
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['first_name', 'last_name']
