e6699d40301f6fd93cc36b9a0d3d4a59cd58e1dc
nh-99 <ifence9@gmail.com>
2015-08-23 19:19:56 -0400

    Multiple pages for trails

--- search/views.py
+++ search/views.py
@@ -1,13 +1,27 @@
 from django.shortcuts import render
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 from trails.models import Trail
 
 def location(request, location):
 	request.session['searchtype'] = 'location'
 	
-	trails = Trail.objects.filter(location__contains=location)[:50]
+	trail_list = Trail.objects.filter(location__contains=location)[:50]
+        paginator = Paginator(trail_list, 10)
+        
+        total = trail_list.count()
+        
+        page = request.GET.get('page')
+        try:
+            trails = paginator.page(page)
+        except PageNotAnInteger:
+            # If page is not an integer, deliver first page.
+            trails = paginator.page(1)
+        except EmptyPage:
+            # If page is out of range (e.g. 9999), deliver last page of results.
+            trails = paginator.page(paginator.num_pages)
 	
-	args = {'trails': trails, 'location': location, 'searchtype': request.session['searchtype']}
+	args = {'trails': trails, 'location': location, 'searchtype': request.session['searchtype'], 'total': total}
 	
 	return render(request, 'search/results.html', args)
 	
