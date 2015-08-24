4f77a6a51e87c7ff9fa8f8ad70c25f1171bf679c
nh-99 <ifence9@gmail.com>
2015-08-23 21:48:09 -0400

    Email on image approval

--- images/views.py
+++ images/views.py
@@ -1,7 +1,26 @@
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
 
+from django.template import Context
+from django.template.loader import render_to_string, get_template
+from django.core.mail import EmailMessage
+
 from .models import TrailImage
+
+# Method to send an email once an image is approved
+def send_approval_email(user):
+    subject = "HikeIt: Image Approval"
+    to = [user.email]
+    from_email = 'image-approval@hikeit.me'
+
+    ctx = {
+        'user': user
+    }
+
+    message = get_template('images/email/approved.html').render(Context(ctx))
+    msg = EmailMessage(subject, message, to=to, from_email=from_email)
+    msg.content_subtype = 'html'
+    msg.send()
 
 def index(request):
     images = TrailImage.objects.filter(approved=False)
@@ -12,4 +31,5 @@ def approve(request, image_id):
         image = TrailImage.objects.get(pk=image_id)
         image.approved = True
         image.save()
+        send_approval_email(image.user)
         return HttpResponseRedirect('/image/')
