bbdb661dcd46f42986e733147ed09477a92a9458
nh-99 <ifence9@gmail.com>
2015-09-21 21:51:20 -0400

    Fixes for admin backend

--- paths/models.py
+++ paths/models.py
@@ -6,4 +6,6 @@ class Path(models.Model):
     path_file = models.FileField(upload_to='paths')
     user = models.ForeignKey(User)
     trail = models.ForeignKey(Trail)
-	
+    
+    def __str__(self):
+	return "user: {0} trail: {1}".format(self.user.username, self.trail.name)
