8780e991c28b1c8c2ed744f6becc463d6eea8930
nh-99 <ifence9@gmail.com>
2015-10-09 22:40:45 -0400

    [WIP] Privacy policy and ToS

--- static_pages/views.py
+++ static_pages/views.py
@@ -2,3 +2,9 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'static_pages/index.html')
+
+def tos(request):
+    return render(request, 'static_pages/tos.html')
+    
+def privacy(request):
+    return render(request, 'static_pages/privacy.html')
