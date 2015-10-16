3ee0f06694aba7f03ac6b1e776353fc842083698
nh-99 <ifence9@gmail.com>
2015-10-15 23:54:16 -0400

    Production server fix

--- HikeIt/wsgi.py
+++ HikeIt/wsgi.py
@@ -13,6 +13,6 @@ from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "HikeIt.settings.active")
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-sys.path.insert(0, os.path.join(BASE_DIR, 'HikeIt/HikeIt/apps'))
+sys.path.insert(0, os.path.join(BASE_DIR, '/apps'))
 
 application = get_wsgi_application()
