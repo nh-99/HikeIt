4eb04a96d5b8ec4faa01e71eb97b3974aa5382f3
nh-99 <ifence9@gmail.com>
2015-10-16 00:31:05 -0400

    Deployment fixes

---
+++ ansible/roles/django/templates/manage.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+import os
+import sys
+
+if __name__ == "__main__":
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "HikeIt.settings.active")
+    BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+    sys.path.insert(0, os.path.join(BASE_DIR, '{{mode}}/HikeIt/apps'))
+
+    from django.core.management import execute_from_command_line
+
+    execute_from_command_line(sys.argv)
