36e7c062a2a4dd3eaa823ba83070fb3254ffcea5
nh-99 <ifence9@gmail.com>
2015-10-15 23:32:19 -0400

    Module restructure

--- manage.py
+++ manage.py
@@ -4,6 +4,8 @@ import sys
 
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "HikeIt.settings.active")
+    BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+    sys.path.insert(0, os.path.join(BASE_DIR, 'HikeIt/HikeIt/apps'))
 
     from django.core.management import execute_from_command_line
 
