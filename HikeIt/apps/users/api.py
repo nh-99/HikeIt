e131472f8f9d194f0371ff1f70e9ad07f1a75ddb
nh-99 <ifence9@gmail.com>
2015-12-29 12:21:37 -0500

    More indentation issues fixed

--- HikeIt/apps/users/api.py
+++ HikeIt/apps/users/api.py
@@ -19,39 +19,39 @@ class UserInfo(APIView):
     permission_classes = (permissions.IsAuthenticated,)
     
     def get(self, request, format=None):
-		if request.user.is_authenticated():
-			user = request.user
-			serialized_user = UserSerializer(user)
-			return Response(serialized_user.data)
-		else:
-			return Response(json.dumps({"result":"Not authenticated"}))
+        if request.user.is_authenticated():
+            user = request.user
+            serialized_user = UserSerializer(user)
+            return Response(serialized_user.data)
+        else:
+            return Response(json.dumps({"result":"Not authenticated"}))
 
 class Register(APIView):
-	"""
-	Register a user
-	"""
-	permission_classes = (permissions.AllowAny,)
-	parser_classes = (JSONParser,)
-	
-	def post(self, request, format=None):
-		username = request.data.get('username')
-		email = request.data.get('email')
-		password = request.data.get('password')
-		token = uuid.uuid1().hex
-		if not request.user.is_authenticated():
-			try:
-				user = User.objects.create_user(username=username, email=email, password=password)
-			except ValueError:
-				return Response({"result":"Malformed request", "data":request.data})
-			except IntegrityError:
-				return Response('{"result":"There was an error creating the user"}')
-			user.is_active = False
-			user.profile.token = token
-			user.save()
-			views.send_confirm_email(user, user.profile.token)
-			return Response(json.dumps({"result":"Confirmation email sent"}))
-		else:
-			return Response(json.dumps({"result":"Already signed in"}))
+    """
+    Register a user
+    """
+    permission_classes = (permissions.AllowAny,)
+    parser_classes = (JSONParser,)
+    
+    def post(self, request, format=None):
+        username = request.data.get('username')
+        email = request.data.get('email')
+        password = request.data.get('password')
+        token = uuid.uuid1().hex
+        if not request.user.is_authenticated():
+            try:
+                user = User.objects.create_user(username=username, email=email, password=password)
+            except ValueError:
+                return Response({"result":"Malformed request", "data":request.data})
+            except IntegrityError:
+                return Response('{"result":"There was an error creating the user"}')
+            user.is_active = False
+            user.profile.token = token
+            user.save()
+            views.send_confirm_email(user, user.profile.token)
+            return Response(json.dumps({"result":"Confirmation email sent"}))
+        else:
+            return Response(json.dumps({"result":"Already signed in"}))
 
 class TimelineToken(APIView):
     """
@@ -61,7 +61,7 @@ class TimelineToken(APIView):
     parser_classes = (JSONParser,)
     
     def post(self, request, format=None):
-		if request.user.is_authenticated():
+        if request.user.is_authenticated():
             user = request.user
             if user.profile.timeline_token != None:
                 return Response(json.dumps({"result": "Token already exists"})
